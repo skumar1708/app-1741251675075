1c731ef72deffcc717c180155f7d4509cbad2cc3
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-06 14:32:26 +0530

    Automated commit: frontend/src/components/Footer.js

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; 2023 Snack Game. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
